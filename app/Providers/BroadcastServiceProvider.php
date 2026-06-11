a05dd7938005cde7aa4bd7b69c88b4fa57579c43
SFenrir20 <SFenrir20@users.noreply.github.com>
2026-06-11 16:04:56 -0500

    fix: prefix api en broadcast routes

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -9,7 +9,7 @@ class BroadcastServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Broadcast::routes(['middleware' => ['auth.api']]);
+        Broadcast::routes(['middleware' => ['auth.api'], 'prefix' => 'api']);
 
         require base_path('routes/channels.php');
     }
